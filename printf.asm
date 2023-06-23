e37310fa216d24811c67010f5660403be5ffb23d
Damian Bernat <48223251+damdam6680@users.noreply.github.com>
2023-06-23 17:10:52 +0200

    Update printf.asm

--- printf.asm
+++ printf.asm
@@ -1,21 +1,45 @@
-         [bits 32]
+[bits 32]
 
 ;        esp -> [ret]  ; ret - adres powrotu do asmloader
 
-         call getaddr ; push on the stack the runtime address of format and jump to getaddr
+call getaddr  ; push on the stack the runtime address of format and jump to getaddr
 format:
-         db 'Hello world!', 0xA, 0
+db 'znak = ', 0
 getaddr:
 
 ;        esp -> [format][ret]
 
-         call [ebx+3*4]  ; printf('Hello world!\n');
-         add esp, 4      ; esp = esp + 4
+call [ebx+3*4]  ; printf('znak = ');
+add esp, 4      ; esp = esp + 4
+
+get_input:
+call [ebx+2*4]  ; eax = getchar();
+cmp eax, 0xA    ; check if the character is a newline
+jz end_program  ; if newline, end the program
 
 ;        esp -> [ret]
 
-         push 0          ; esp -> [0][ret]
-         call [ebx+0*4]  ; exit(0);
+push eax  ; eax -> stack
+
+;        esp -> [eax][ret]
+
+call getaddr2  ; push on the stack the runtime address of format and jump to getaddr
+format2:
+db '%c', 0
+getaddr2:
+
+;        esp -> [format2][eax][ret]
+
+call [ebx+3*4]  ; printf('ascii = %d\n', eax);
+add esp, 2*4    ; esp = esp + 8
+
+;        esp -> [ret]
+
+jmp get_input   ; jump back to get_input to continue reading characters
+
+end_program:
+push 0          ; esp -> [0][ret]
+call [ebx+0*4]  ; exit(0);
 
 ; asmloader API
 ;
@@ -33,7 +57,7 @@ getaddr:
 ; 3 - printf
 ; 4 - scanf
 ;
-; To co funkcja zwróci jest w EAX.
+; To co funkcja zwrÃ³ci jest w EAX.
 ; Po wywolaniu funkcji sciagamy argumenty ze stosu.
 ;
 ; https://gynvael.coldwind.pl/?id=387
