c0a38e0fe23d290267004dae4f33110273de8516
Damian Bernat <48223251+damdam6680@users.noreply.github.com>
2023-06-25 18:28:52 +0200

    Update PROJEKT.asm

--- PROJEKT.asm
+++ PROJEKT.asm
@@ -1,71 +1,86 @@
         [bits 32]
+        
+;        esp -> [ret]  ; ret - adres powrotu do asmloader
 
 N        equ 100
 length   equ 0
 
-         sub esp, N+1
+         sub esp, N+1  ; esp = esp + N+1
 
          call getaddr  ; push on the stack the runtime address of format and jump to getaddr
 format:
          db 'zdanie: ', 0
 getaddr:
-         call [ebx+3*4]  ; printf('ascii = %d\n', eax);
-         add esp, 2*4    ; esp = esp + 8
+
+;        esp -> [format][ret]
+
+         call [ebx+3*4]  ; printf(format);
+         add esp, 4      ; esp = esp + 4
+
+;        esp -> [ret]
 
          mov esi,0  ; esi = 0
 
 _loop:
-         cmp esi,N
-         jg _end
-         call [ebx+2*4]  ; eax = getchar();
-         cmp eax,0xA
-         je _end
-         push eax
-         inc esi
-         jmp _loop
+         cmp esi, N      ; eax - N          ; OF SF ZF AF PF CF affected
+         jg _end         ; jump if greater  ; jump if SF == OF and ZF = 0
+         call [ebx+2*4]  ; eax = getchar()
+         cmp eax, 0xA    ; eax - 0xA          ; OF SF ZF AF PF CF affected
+         je _end         ; jump if equal
+
+         push eax        ; esp -> eax
+
+;        esp -> [eax][ret]
+
+         inc esi    ; esi++
+         jmp _loop  ; jump always
 
 _end:
-         mov edx,esi
-         mov edi,0
-         dec esi
-         mov ecx, esi
+         mov edx, esi  ; edx = esi
+         mov edi, 0    ; edi = 0
+         dec esi       ; esi--
+         mov ecx, esi  ; ecx = esi
 
 _loop2:
-         mov al, [esp+edi*4]
-         mov ah, [esp+ecx*4]
+         mov al, [esp+edi*4]  ; al = *(char*)edi*4
+         mov ah, [esp+ecx*4]  ; ah = *(char*)ecx*4
 
-         cmp ah, 32     ; compare ah to space
+         cmp ah, 32             ; compare ah to space
          je check_for_space_ah  ; jump if equal
 
-         cmp al, 32    ; compare al to space
+         cmp al, 32             ; compare al to space
          je check_for_space_al  ; jump if equal
 
-         cmp al,90
-         jge tolower
-         add al,32
+         cmp al,90    ; compare al to 90 ASCII
+         jge tolower  ; jump if greater or equal ; jump if SF == OF or ZF = 1
+         add al,32    ; al += 32
 
 tolower:
-         cmp ah, 90  ; compare al to 90 ASCII
-         jge if_palindrom    ; jump if greater or equal
-         add ah, 32  ; ah += 32
+         cmp ah, 90        ; compare al to 90 ASCII
+         jge if_palindrom  ; jump if greater or equal
+         add ah, 32        ; ah += 32
 
 if_palindrom:
-         cmp al, ah
-         jne done
+         cmp al, ah  ; al - ah  ; OF SF ZF AF PF CF affected
+         jne done    ; jump if not equal
 
-         inc edi
-         dec ecx
+         inc edi  ; edi++
+         dec ecx  ; ecx--
 
-         cmp edi, edx
-         jne _loop2
+         cmp edi, edx  ; edi - edx  ; OF SF ZF AF PF CF affected
+         jne _loop2    ; jump if not equal
 
          call getaddr2  ; push on the stack the runtime address of format and jump to getaddr
 format2:
          db 0xA, 'zdanie jest palindromem', 0xA, 0
 getaddr2:
 
-         call [ebx+3*4]  ; printf('ascii = %d\n', eax);
+;        esp -> [format2][eax][ret]
+
+         call [ebx+3*4]  ; printf(format2);
          add esp, 2*4    ; esp = esp + 8
+
+;        esp -> [ret]
 
          push 0          ; esp -> [0][ret]
          call [ebx+0*4]  ; exit(0)
@@ -76,16 +91,52 @@ done:
 format3:
          db 0xA, 'zdanie nie jest palindromem', 0xA, 0
 getaddr3:
-         call [ebx+3*4]  ; printf('ascii = %d\n', eax);
+
+;        esp -> [format3][eax][ret]
+
+         call [ebx+3*4]  ; printf(format3);
          add esp, 2*4    ; esp = esp + 8
+         
+;        esp -> [ret]
 
          push 0          ; esp -> [0][ret]
          call [ebx+0*4]  ; exit(0)
 
 check_for_space_ah:
-         dec ecx    ; edi--
+         dec ecx     ; edi--
          jmp _loop2  ; jump always
 
 check_for_space_al:
-         inc edi    ; esi++
+         inc edi     ; esi++
          jmp _loop2  ; jump always
+         
+; asmloader API
+;
+; ESP wskazuje na prawidlowy stos
+; argumenty funkcji wrzucamy na stos
+; EBX zawiera pointer na tablice API
+;
+; call [ebx + NR_FUNKCJI*4] ; wywolanie funkcji API
+;
+; NR_FUNKCJI:
+;
+; 0 - exit
+; 1 - putchar
+; 2 - getchar
+; 3 - printf
+; 4 - scanf
+;
+; To co funkcja zwróci jest w EAX.
+; Po wywolaniu funkcji sciagamy argumenty ze stosu.
+;
+; https://gynvael.coldwind.pl/?id=387
+
+%ifdef COMMENT
+
+ebx    -> [ ][ ][ ][ ] -> exit
+ebx+4  -> [ ][ ][ ][ ] -> putchar
+ebx+8  -> [ ][ ][ ][ ] -> getchar
+ebx+12 -> [ ][ ][ ][ ] -> printf
+ebx+16 -> [ ][ ][ ][ ] -> scanf
+
+%endif
